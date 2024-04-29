60e5c5d73fd5c03c52e61a79ca99f31679eb6cc4
Johnfernandez117 <145405174+JohnAlexanderFernandez@users.noreply.github.com>
2024-04-29 14:27:42 -0500

    Update Home.jsx

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 50,
+  },
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+});
 
 export default function Home() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={{fontSize: 30, textAlign: 'center', marginTop: '20%'}}>
         HOME
       </Text>
     </View>
   );
 }
+
